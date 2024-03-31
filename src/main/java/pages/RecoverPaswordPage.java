b106126fdc4c1900295d7cf5eeaa72b3a0847cda
Roman Mazaev <jupiter836@yandex.ru>
2024-03-31 12:28:20 +0300

    refactor

--- src/main/java/pages/RecoverPaswordPage.java
+++ src/main/java/pages/RecoverPaswordPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -9,6 +10,7 @@ public class RecoverPaswordPage extends BasePage{
         super(driver);
     }
 
+    @Step("Нажать на ссылку входа")
     public LoginPage clickLinkLogin() {
         driver.findElement(linkLogin).click();
         return new LoginPage(driver);
